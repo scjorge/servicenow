aa432dc4bc6f5741cbb5fd5a272794bdbe2ffa9f
Jorge Silva da Costa <jorge.costa@comunix.net.br>
2025-09-17 13:11:23 -0300

    update

---
+++ scritps/scheduled/fechar_incidents.js
@@ -0,0 +1,28 @@
+/* 
+| Valor (`state`) | Label (Rótulo exibido) |
+| --------------- | ---------------------- |
+| **1**           | New                    |
+| **2**           | In Progress            |
+| **3**           | On Hold                |
+| **6**           | Resolved               |
+| **7**           | Closed                 |
+| **8**           | Canceled               |
+*/
+
+
+(function() {
+    var gr = new GlideRecord("incident");
+    gr.addQuery("state", "Resolved");  // somente incidentes resolvidos
+    gr.addQuery("resolved_at", "<=", gs.daysAgoStart(7)); // mais de 7 dias
+    gr.query();
+
+    var count = 0;
+    while (gr.next()) {
+        gr.state = 7; // Closed
+        gr.update();
+        count++;
+    }
+
+    // Registrar log no system log
+    gs.log("Scheduled Job: " + count + " incidentes fechados automaticamente.", "CloseResolvedIncidents");
+})();
